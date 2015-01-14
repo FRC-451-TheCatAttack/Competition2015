be34a4f157459dbda8fc60a599c89d2dd5136e31
Trenton <Trenton@10.9.32.145>
2015-01-13 20:01:17 -0500

    Added new comment to the auto class

--- Competition2015/src/org/usfirst/frc/team451/robot/Auto.java
+++ Competition2015/src/org/usfirst/frc/team451/robot/Auto.java
@@ -2,7 +2,8 @@ package org.usfirst.frc.team451.robot;
 
 /**
  *This is the Auto class.  This class is in charge of the Robot's movements and actions
- *during the autonomous period of the match.
+ *during the autonomous period of the match. These actions will include moving and possibly lifting 
+ *stuff.
  *
  */
 public class Auto {
